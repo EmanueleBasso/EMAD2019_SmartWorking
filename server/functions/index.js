b9ac90da9d2ef1ef3bf6031553216857b55298c7
Emanuele Basso <bassoemanuele1704@gmail.com>
2019-11-21 13:08:03 +0100

    Implementata la cloud function per la verifica del piano di SW

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const firebase = require("firebase/app");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const checkSWAlreadyEnteredModule = require('./src/sw/checkSWAlreadyEntered');
+
+module.exports = {
+    'checkSWAlreadyEntered': functions.region('europe-west1').https.onRequest(checkSWAlreadyEnteredModule)
+};
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCFS_Kx9pZVoYinGyO92SKM_HEgWPdHK7Q',
+    authDomain: 'smart-working-5f3ea.firebaseapp.com',
+    databaseURL: 'https://smart-working-5f3ea.firebaseio.com',
+    projectId: 'smart-working-5f3ea',
+    storageBucket: 'smart-working-5f3ea.appspot.com',
+    messagingSenderId: '1072133615988',
+    appId: '1:1072133615988:web:6f748c0c8a537e7ff4bc21',
+    measurementId: 'G-HR9ZDFD32P'
+  };
+firebase.initializeApp(firebaseConfig);
---
+++ server/functions/src/sw/checkSWAlreadyEntered.js
@@ -0,0 +1,39 @@
+const firebase = require("firebase/app");
+require("firebase/firestore");
+
+module.exports = async (request, response) => {
+    var uid = request.query.uid
+
+    if(uid === undefined){
+        response.send({hasError: true, error: "UID undefined"})
+    }
+
+    var db = firebase.firestore()
+    await db.collection('SmartWorking').where('dipendente', '==', uid).get().then( (snapshot) => {
+        arrayDate = []
+        snapshot.forEach(elem => {
+            arrayDate.push(elem.data().data)
+        });
+
+        arrayDateSorted = arrayDate.sort(function(o1, o2) {
+            arrayData1 = o1.split('-')
+            data1 = new Date(arrayData1[2], arrayData1[1], arrayData1[0])
+            arrayData2 = o2.split('-')
+            data2 = new Date(arrayData2[2], arrayData2[1], arrayData2[0])
+
+            if (data1.getTime() < data2.getTime())    return -1;
+            else if(data1.getTime() > data2.getTime()) return  1;
+            else                      return  0;
+        });
+
+        var lastMonthEntered = arrayDateSorted.pop().split('-')[1]
+
+        var month = (new Date()).getMonth() + 2
+
+        if(lastMonthEntered == month){
+            response.send({alreadyEntered: true})
+        }else{
+            response.send({alreadyEntered: false}) 
+        }
+    });
+};
