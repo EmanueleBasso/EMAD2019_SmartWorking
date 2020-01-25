4e5923d6b94caa6657f371fece370396b99bcb86
ClaraMonaco <32262876+ClaraMonaco@users.noreply.github.com>
2020-01-25 19:02:15 +0100

    Aggiunta dei controlli per le form relative a dipendenti e progetti
    Aggiunta dei controlli per le form relative all'inserimento di dipendenti e progetti.

--- amministratore/src/app/form-progetti/form-progetti.page.ts
+++ amministratore/src/app/form-progetti/form-progetti.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import LoadingService from '../providers/loading.service';
+import { NgForm } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
+import { isString, isNumber } from 'util';
 
 @Component({
   selector: 'app-form-progetti',
@@ -7,9 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormProgettiPage implements OnInit {
 
-  constructor() { }
+  constructor(private loadingService: LoadingService, private alertController: AlertController, private navCtrl: NavController) { }
 
+
+  
+  async presentAlertEmail() {
+    const alert = await this.alertController.create({
+      header: 'Credenziali non valide!',
+      message: 'Inserire email menager correttamente',
+      cssClass: 'alertClass2',
+      buttons: [{
+        text: 'OK',
+      }]
+    });
+    await alert.present();
+  }
+  
   ngOnInit() {
   }
+
+  onSubmit(form: NgForm) {
+    let manager1: string ="";
+    const scope= form.value.scope;
+    const description= form.value.description;
+    const manager= form.value.manager;
+    const state= form.value.state;
+    console.log(scope,description,manager,state);
+     
+    if (state === 'false' || state === false) {
+      console.log('non attivo');
+      // progetto attivo
+    } else {
+      // progetto chiuso
+      console.log('attivo');
+    }
+
+    if (!manager.includes('@')) {
+      manager1 = manager.concat('@capgemini.com');
+    }
+    console.log(manager1);
+
+    if (!(manager.includes('@capgemini.com') || manager1.includes('@capgemini.com')) && !manager.startsWith('@')) {
+    
+    this.presentAlertEmail();
+  }
+  
+    if(parseFloat(manager.charAt(0))) {
+    this.presentAlertEmail();
+  }
+
+    console.log(state );
+
+   
+    this.navCtrl.navigateRoot('/progetti');
+    
+
+  }
+
 
 }
